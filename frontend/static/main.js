81f579a1d93a1377c85d442ee2dabafc5f3f5da0
Amadeus <vasiavasia10000@gmail.com>
2024-06-28 19:02:21 +0300

    Final design

--- frontend/static/main.js
+++ frontend/static/main.js
@@ -11,22 +11,25 @@ function createPlot(table, labels, openPrices) {
     };
 
     var layout = {
-        title: `Currency Exchange Rates for ${table}`,
-        titlefont: { color: 'white' },
         paper_bgcolor: '#343a40',
         plot_bgcolor: '#343a40',
         xaxis: {
-            title: 'Time',
             type: 'date',
             autorange: true,
             fixedrange: true,
             color: 'white'
         },
         yaxis: {
-            title: 'Open Price',
             autorange: true,
             fixedrange: true,
             color: 'white'
+        },
+        margin: {
+            l: 50,
+            r: 20,
+            b: 50,
+            t: 20,
+            pad: 4
         }
     };
 
@@ -69,5 +72,17 @@ function updateRange(table, range) {
         .then(response => response.json())
         .then(data => {
             createPlot(table, data.labels, data.open_prices);
+            updateButtonStyles(table, range);
         });
+}
+
+function updateButtonStyles(table, activeRange) {
+    const buttonGroup = document.querySelector(`#chart-${table}`).closest('.card-body').querySelector('.btn-group');
+    buttonGroup.querySelectorAll('.btn').forEach(button => {
+        if (button.getAttribute('onclick').includes(activeRange)) {
+            button.classList.add('btn-active');
+        } else {
+            button.classList.remove('btn-active');
+        }
+    });
 }
